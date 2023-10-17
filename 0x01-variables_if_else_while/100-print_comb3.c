c0cb6014ab5fbc8ee70c608b0eb8ef7d0e967059
wesacoke <princewesa@gmail.com>
2023-10-17 12:08:16 -0700

    variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,12 +4,13 @@
  * main - Prints all possible different combinations of two digits
  * Return: Always 0(Success)
  */
-int main (void)
+int main(void)
 {
 	int n, m;
-	for (n = 48; n <= 56; n++)
+
+	for (n = 48; n <= 57; n++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (m = 48; m <= 57; m++)
 		{
 			putchar(n);
 			putchar(m);
