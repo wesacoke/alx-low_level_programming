a560f1e95a30655a80dd2f82a1bc4d894db09cdc
wesacoke <princewesa@gmail.com>
2023-10-17 18:42:43 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- * separated by a comma followed by a space.
+ * main - Prints first 50 Fibonacci number
  *
  * Return: Always 0.
  */
@@ -11,7 +10,7 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0; count < 50; count++)
+	for (count = 2; count < 50; count++)
 	{
 		sum = fib1 + fib2;
 		fib1 = fib2;
