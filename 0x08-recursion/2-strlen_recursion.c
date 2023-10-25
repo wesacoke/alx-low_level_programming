f6206a57f43120902602d71a2a0c2b5009a8b11b
wesacoke <princewesa@gmail.com>
2023-10-25 10:40:50 -0700

    Dreams feel real while we're in them.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - main function
+  *
+  * @s: Function parameter
+  *
+  * Return: Length
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
