a0f6509835e505b9fda9b1d80d98b152d821c3d1
wesacoke <princewsa@gmail.com>
2023-10-27 15:46:06 -0700

    neither irony nor sarcasm is argument

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
 {
 	int j;
 
-	for (j = 0; j < argc; j++)		
+	for (j = 0; j < argc; j++)
 	{
-		printf("%s\n", argv[j]);		
+		printf("%s\n", argv[j]);
 	}
 	return (0);
 }
