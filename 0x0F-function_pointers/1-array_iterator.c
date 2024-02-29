4a0ce127b5f79ebadd4168ecc16ccab22144d222
wesacoke <princewsa@gmail.com>
2024-02-29 15:45:20 -0800

    0x0F. C - Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
-/*
- * array_iterator - prints each array element on a new line
+/**
+ * array_iterator - prints each array elem on a newl
  * @array: array
- * @size: how many elements to print
- * @action: pointer to a function that prints in regular or hex
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
