49a61b7f050381f92b463c01a38c52df275efc39
wesacoke <princewsa@gmail.com>
2024-02-29 16:18:42 -0800

    vardiac function tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,7 +6,7 @@
  * sum_them_all - Returns the sum of all its paramters.
  * @n: The number of paramters passed to the function.
  * @...: A variable number of paramters to calculate the sum of.
- * 
+ *
  * Return: If n == 0 - 0.
  * Otherwise - the sum of all parameters.
  */
