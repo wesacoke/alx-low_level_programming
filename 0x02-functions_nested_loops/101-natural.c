1b519749eaf34c5f12ff2d43675a0a20eaf56f01
wesacoke <wesaemmanuel02@gmail.com>
2024-03-11 13:46:32 -0700

    corrections

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,29 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 
 /**
- * main - Prints the first 52 fib
- * Return: 0 (succss)
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
+
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
-
-	while (i < 50)
-	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i ==1)
-			printf(", %ld" k);
-		else
-		{
-			k += j;
-			j = k -j;
-			printf(", %ld" k);
-		}
-		++i;
-	}
-	printf("\n");
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
 	return (0);
 }
